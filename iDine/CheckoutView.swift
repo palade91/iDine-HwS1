ea006442023fd92d9c94803d1df338cc4eae659c
Catalin Palade <palade91@yahoo.com>
2020-03-24 15:47:08 +0000

    binding

---
+++ iDine/CheckoutView.swift
@@ -0,0 +1,38 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by Catalin Palade on 24/03/2020.
+//  Copyright © 2020 Catalin Palade. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    
+    @EnvironmentObject var order: Order
+    
+    @State private var paymentType = 0
+    
+    static let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    
+    var body: some View {
+        VStack {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(0..<Self.paymentTypes.count) {
+                        Text(Self.paymentTypes[$0])
+                    }
+                }
+            }
+        }
+        .navigationBarTitle(Text("Payment"), displayMode: .inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static let order = Order()
+    static var previews: some View {
+        CheckoutView().environmentObject(order)
+    }
+}
